14eaf5d52674298c0ee893f5b7c13462976e928b
gcampbell391 <gcampbell391@gmail.com>
2020-11-17 12:30:43 -0500

    added custom loader

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from "react"
 import Footer from "./Footer"
 import peyton from '../peyton.svg'
+import galaxy from '../galaxy.svg'
+
 
 
 
 const Home = () => {
 
     const [dailyImage, setDailyImage] = useState()
+
 
     useEffect(() => {
         fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}`)
@@ -19,7 +22,10 @@ const Home = () => {
 
     if (dailyImage === undefined) {
         return (
-            <h1 className="shimmer" >Daily Star Gaze</h1>
+            <div className='home-not-loaded'>
+                <h1 className="shimmer2" >Fetching Daily Image!</h1>
+                <img src={galaxy} id='galaxy' alt='galaxy-loader' />
+            </div>
         )
     }
 
@@ -33,7 +39,7 @@ const Home = () => {
             </div>
             <hr id='hr' />
             <p id='star-title'>Created And Designed For My Niece. May You Continue To Reach For The Stars And Follow Your Dreams!</p>
-            <img src={peyton} id='peyton-star' />
+            <img src={peyton} id='peyton-star' alt='Peyton-Star' />
             <p id='updated-title'>Updated On: {dailyImage.date.split('-')[1]}/{dailyImage.date.split('-')[2]}/{dailyImage.date.split('-')[0]}</p>
             <Footer />
         </div>
